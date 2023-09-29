ba3733f3b9eb69f6696a6651220a1d4d4f2c32e4
unknown <gonza.cunet@outlook.com>
2023-09-29 11:07:37 -0300

    desafio terminado

--- componentes/footer.js
+++ componentes/footer.js
@@ -4,9 +4,9 @@ function footerAdd(e) {
 
   componentEl.innerHTML = `<img class="footer__logo" src="./Images/Gonza-logo.png" alt="">
   <div class="links__container"> 
-      <div class="ind__link"><img src="./Images/home.png" alt=""><a class="footer__links" href="/index.html">Home</a></div>
-      <div class="ind__link"><img src="./Images/user.png" alt=""><a class="footer__links" href="/servicios.html">Servicios</a></div>
-      <div class="ind__link"><img src="./Images/phone.png" alt=""><a class="footer__links" href="/contacto.html">Contacto</a></div>
+      <div class="ind__link"><img src="./Images/home.png" alt=""><a class="footer__links" href="./index.html">Home</a></div>
+      <div class="ind__link"><img src="./Images/user.png" alt=""><a class="footer__links" href="./servicios.html">Servicios</a></div>
+      <div class="ind__link"><img src="./Images/phone.png" alt=""><a class="footer__links" href="./contacto.html">Contacto</a></div>
   </div>
   <div class="social-media"><img class="social__icons" src="./Images/Linkedin.png" alt="">
   <img class="social__icons" src="./Images/github.png" alt="">
